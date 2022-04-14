be7e6990b7e01e636efd7dfbc78d621f3cf5bcfe
hira480 <hiramannan8@gmail.com>
2022-04-15 01:13:23 +0600

    login form added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -14,15 +14,17 @@ const Services = () => {
     }, [])
 
     return (
-        <div className='container'>
-            <h1 className='text-primary text-center mt-5'>Our Services</h1>
-            <div className='services-container'>
-                {
-                    services.map(service => <Service
-                        key={service.id}
-                        service={service}
-                    ></Service>)
-                }
+        <div id='services' className='container'>
+            <div className="row">
+                <h1 className='text-primary text-center mt-5'>Our Services</h1>
+                <div className='services-container'>
+                    {
+                        services.map(service => <Service
+                            key={service.id}
+                            service={service}
+                        ></Service>)
+                    }
+                </div>
             </div>
         </div>
     );
