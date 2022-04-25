d60622d4e327919dbcbbb9defb476a40f15b73b0
hira480 <hiramannan8@gmail.com>
2022-04-25 12:35:14 +0600

    delete service data from ui and database

---
+++ src/Pages/ManageServices/ManageServices.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import useServices from '../../hooks/useServices';
+
+const ManageServices = () => {
+    const [services, setServices] = useServices();
+
+    const handelDelete = id => {
+        const proceed = window.confirm('Are you Sure you want to delete it?');
+        if (proceed) {
+            const url = `http://localhost:5000/service/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = services.filter(service => service._id !== id);
+                    setServices(remaining);
+                })
+        }
+    }
+    return (
+        <div className='w-50 mx-auto'>
+            <h2>Manage Your Services</h2>
+            {
+                services.map(service => <div key={service._id}>
+                    <h5>{service.name} <button onClick={() => handelDelete(service._id)}>X</button></h5>
+
+                </div>)
+            }
+        </div>
+    );
+};
+
+export default ManageServices;
