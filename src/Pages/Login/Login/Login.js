83a066c18c0b6c7c35c4a5d0363dc8d41facc6a0
hira480 <hiramannan8@gmail.com>
2022-04-28 15:51:07 +0600

    setup JWT

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -10,6 +10,7 @@ import Loading from '../../../Shared/Loading/Loading';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PageTitel from '../../../Shared/PageTitel/PageTitel';
+import axios from 'axios';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -33,17 +34,20 @@ const Login = () => {
     }
 
     if (user) {
-        navigate(from, { replace: true });
+        // navigate(from, { replace: true });
     }
     if (error) {
         errorElement = <p className='text-danger'>Error: {error?.message}</p>
     }
-    const handelSubmit = event => {
+    const handelSubmit = async event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
-        signInWithEmailAndPassword(email, password);
+        await signInWithEmailAndPassword(email, password);
+        const { data } = await axios.post('http://localhost:5000/login', { email });
+        localStorage.setItem('accessToken', data.accessToken);
+        navigate(from, { replace: true });
     }
 
     const navigateRegister = event => {
